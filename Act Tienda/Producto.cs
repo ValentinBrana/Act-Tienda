cb66b208cf355c471f05c64dd013b24e2627dd0d
Valentin <valentinnbrana@gmail.com>
2025-05-07 22:34:08 -0300

    Agregar archivos de proyecto.

---
+++ Act Tienda/Producto.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act_Tienda
+{
+    public class Producto
+    {
+        private static List<Producto> productos = new List<Producto>();
+        private string nombre;
+        private string descripcion;
+        private int precio;
+        private Categoria categoria;
+        private int cantidadInventario;
+
+        public static List<Producto> Productos { get => productos; set => productos = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public int CantidadEnInventario { get => cantidadInventario; set => cantidadInventario = value; }
+        internal Categoria Categoria { get => categoria; set => categoria = value; }
+
+        public Producto(string nombre, string descripcion, int precio, Categoria categoria, int cantidadEnInventario)
+        {
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.categoria = categoria;
+            this.cantidadInventario = cantidadInventario;
+        }
+
+        public Producto()
+        {
+            nombre = "";
+            descripcion = "";
+            precio = 0;
+            categoria = null;
+            cantidadInventario = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Nombre: " + this.nombre +
+                    "Descripción: " + this.descripcion +
+                    "Precio: " + this.precio +
+                    "Categoria: " + this.Categoria.Nombre +
+                    "Cantidad en inventario: " + this.cantidadInventario;
+        }
+
+        public void Guardar()
+        {
+            Producto.Productos.Add(this);
+        }
+
+        public static void ModificarProducto(string nombre)
+        {
+            nombre = nombre.ToLower().Trim();
+            Producto seleccionado = null;
+
+            foreach (Producto p in Producto.Productos)
+            {
+                if (p.Nombre.ToLower().Trim() == nombre)
+                {
+                    seleccionado = p;
+                    break;
+                }
+            }
+
+            if (seleccionado != null)
+            {
+                Console.WriteLine($"¿Usted desea modificar el producto {seleccionado.Nombre}? S/N");
+                string respuesta = Console.ReadLine()?.ToLower().Trim();
+
+                if (respuesta == "s")
+                {
+                    Console.Write("Ingrese la nueva cantidad en inventario: ");
+                    seleccionado.CantidadEnInventario = int.Parse(Console.ReadLine());
+
+                    Console.Write("Ingrese el nuevo precio: ");
+                    seleccionado.Precio = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Datos del producto actualizados:");
+                    Console.WriteLine(seleccionado.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("No se han realizado modificaciones.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se ha encontrado el producto.");
+            }
+        }
+    }
+}
---
+++ Act Tienda/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act_Tienda
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrecargarDatos();
+
+            Cliente cliente = new Cliente("valentin", "valentinnbrana@gmail.com", null);
+            Carrito carrito = new Carrito(cliente);
+            Orden orden = null;
+
+            int opcion;
+            do
+            {
+                opcion = Menu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el producto que quiera modificar:");
+                        string nombreProducto = Console.ReadLine();
+                        Producto.ModificarProducto(nombreProducto);
+                        break;
+
+                    case 2:
+                        GestionarCarrito(carrito);
+                        break;
+
+                    case 3:
+                        Console.Write("Ingrese la dirección de entrega:");
+                        string direccion = Console.ReadLine();
+                        orden = carrito.RealizarOrden(direccion);
+                        break;
+
+                    case 4:
+                        if (orden != null)
+                        {
+                            orden.ConfirmarOrden();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Primero debe finalizar una compra.");
+                        }
+                        break;
+                }
+
+                Console.WriteLine("\n\nToque una tecla para continuar..");
+                Console.ReadKey();
+
+            } while (opcion != 0);
+
+            Console.ReadKey();
+        }
+
+        static void PrecargarDatos()
+        {
+            Categoria c1 = new Categoria("Comida", "Comestible.");
+            Categoria c2 = new Categoria("Bebida", "Bebible.");
+            Categoria c3 = new Categoria("Cosmetico", "Cosmetologia");
+
+            Producto p1 = new Producto("Lays", "las mas ricardas", 5000, c1, 3);
+            p1.Guardar();
+        }
+
+        private static int Menu()
+        {
+            int opcion;
+
+            Console.WriteLine("TIENDA");
+            Console.WriteLine("1 Modificar un producto");
+            Console.WriteLine("2 Gestionar carrito");
+            Console.WriteLine("3 Confirmar orden");
+            Console.WriteLine("4 Finalizar compra");
+            Console.WriteLine("0 - Salir");
+
+            opcion = Validaciones.LeerInt(0, 4);
+            return opcion;
+        }
+
+        private static void GestionarCarrito(Carrito carrito)
+        {
+            Console.WriteLine("1 - Agregar producto");
+            Console.WriteLine("2 - Eliminar producto");
+            Console.WriteLine("3 - Ver total del carrito");
+            Console.WriteLine("4 - Mostrar productos del carrito");
+            Console.Write("Seleccione una opción: ");
+            string opcionCarrito = Console.ReadLine();
+
+            if (opcionCarrito == "1")
+            {
+                Console.Write("Ingrese el nombre del producto a agregar: ");
+                string nombreAgregar = Console.ReadLine().Trim();
+
+                Producto pAgregar = Producto.Productos.Find(p => p.Nombre.ToLower() == nombreAgregar.ToLower());
+                if (pAgregar != null)
+                    carrito.AgregarProdCarrito(pAgregar);
+                else
+                    Console.WriteLine("Producto no encontrado.");
+            }
+            else if (opcionCarrito == "2")
+            {
+                Console.Write("Ingrese el nombre del producto a eliminar: ");
+                string nombreEliminar = Console.ReadLine().Trim();
+                carrito.EliminarProdCarrito(nombreEliminar);
+            }
+            else if (opcionCarrito == "3")
+            {
+                Console.WriteLine("Total del carrito: " + carrito.CalcularTotal());
+            }
+            else if (opcionCarrito == "4")
+            {
+                carrito.MostrarCarrito();
+            }
+        }
+    }
+}
